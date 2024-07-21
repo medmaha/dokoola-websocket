39b74d545b95c2cf7a48a3077abb29d521bb2efb
medmaha <toure925@outlook.com>
2024-07-21 17:16:56 +0000

    Added index route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -5,7 +5,12 @@ const indexRouter = express.Router();
 // BASE Path: /health
 
 indexRouter.get("/", (_, res) => {
-  res.status(200).json({ message: "OK" });
+  res
+    .status(200)
+    .json({
+      message: "OK",
+      data: "msedge --unsafely-treat-insecure-origin-as-secure=http://your-insecure-domain.com --user-data-dir=/tmp/temporary-edge-profile",
+    });
 });
 
 export default indexRouter;
