4a56f6b1c87538d5a7740e83d6f2df5a54de77da
medmaha <toure925@outlook.com>
2026-05-06 17:38:38 +0000

    feat: implement mutex for thread-safe WebsocketConn methods

--- pkg/config.go
+++ pkg/config.go
@@ -1,6 +1,9 @@
 package pkg
 
 import (
+	"sync"
+	"time"
+
 	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
 )
@@ -14,8 +17,33 @@ type WebsocketConn struct {
 	SocketID    string
 	StartTime   time.Time
 	Connenction *websocket.Conn
+	writeMu     sync.Mutex
+}
+
+func (s *WebsocketConn) WriteJSON(v any) error {
+	s.writeMu.Lock()
+	defer s.writeMu.Unlock()
+	return s.Connenction.WriteJSON(v)
+}
+
+func (s *WebsocketConn) WriteMessage(msgType int, data []byte) error {
+	s.writeMu.Lock()
+	defer s.writeMu.Unlock()
+	return s.Connenction.WriteMessage(msgType, data)
+}
+
+func (s *WebsocketConn) ReadJSON(v any) error {
+	return s.Connenction.ReadJSON(v)
+}
+
+func (s *WebsocketConn) SetReadDeadline(t time.Time) error {
+	return s.Connenction.SetReadDeadline(t)
+}
+
+func (s *WebsocketConn) Close() error {
+	return s.Connenction.Close()
 }
 
 type JSONConn interface {
-	WriteJSON(v interface{}) error
+	WriteJSON(v any) error
 }
